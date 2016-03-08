a260c072258bc6186a8865dd7cfbfc388334bdbf
fernandocardenasm <fernando.cardenasm@gmail.com>
2016-03-07 22:44:05 -0500

    Add Deposit Controller

---
+++ VendingMachine/DepositController.swift
@@ -0,0 +1,25 @@
+//
+//  DepositController.swift
+//  VendingMachine
+//
+//  Created by Fernando Cardenas on 7/03/16.
+//  Copyright © 2016 Treehouse. All rights reserved.
+//
+
+import UIKit
+
+class DepositController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func dismiss() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+}
